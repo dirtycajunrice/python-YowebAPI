b596da476081018605c2fa704bcb248a8150bb92
dirtycajunrice <nick@cajun.pro>
2019-01-20 11:59:16 -0600

    add requirements.txt

--- yoweb/helpers.py
+++ yoweb/helpers.py
@@ -2,6 +2,36 @@ BASIC_ATTRS = ('rank', 'name')
 SHARES = ('jobbing_pirate', 'cabin_person', 'pirate', 'officer', 'fleet_officer', 'senior_officer', 'captain')
 RANKS = ['Captain', 'Senior Officer', 'Fleet Officer', 'Officer', 'Pirate', 'Cabin Person', 'Jobbing Pirate']
 
+ULTIMATE_MAPPINGS = {
+    'piracy': {
+        'swordfighting': 0,
+        'bilging': 2,
+        'sailing': 3,
+        'rigging': 31,
+        'navigating': 4,
+        'battle_navigation': 29,
+        'gunning': 5,
+        'carpentry': 6,
+        'rumble': 27,
+        'treasure_haul': 30,
+        'patching': 32,
+    },
+    'crafting': {
+        'distilling': 14,
+        'alchemistry': 18,
+        'shipwrightery': 16,
+        'blacksmithing': 15,
+        'foraging': 12,
+        'weaving': 13,
+    },
+    'carousing': {
+        'drinking': 1,
+        'spades': 21,
+        'hearts': 24,
+        'treasure_drop': 25,
+        'poker': 26
+    }
+}
 
 def clean_stat(data):
     stats = data.split('  ')
